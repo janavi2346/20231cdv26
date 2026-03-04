3569afc6f551ed88712ab2909500b7e89f2bd48d
jahnavi <gjahnavireddy1@gmail.com>
2026-03-04 11:54:21 +0530

    Update and rename Add.java to EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,11 @@
+public class EvenOdd{
+    public static void main(String[] args) {
+        int number = 15;
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+    }
+}
